c7d7705ce6aaa3346f63672db6dbe6705a5ff170
Andrew Weber <andrew.robert.weber@gmail.com>
2015-07-08 21:06:45 -0700

    rotates burgers

---
+++ src/rotateBurgers.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	var rotation = 0
+  $('body').on('click','.gifDancer', function(event){
+    context = this;
+    var spins = 0
+    var spin = function(context){
+      if(spins < 50){
+        rotation += 10;
+        $(context).rotate(rotation);
+        spins++;
+        setTimeout(function(){spin(context);}, 50);
+      } 
+    }
+    spin(context);
+	});
+  $.fn.rotate = function(degrees) {
+    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                 '-moz-transform' : 'rotate('+ degrees +'deg)',
+                 '-ms-transform' : 'rotate('+ degrees +'deg)',
+                 'transform' : 'rotate('+ degrees +'deg)'});
+  };
+});
